cdfd39368b95b3957fbed97b655cdc2c420c6f9f
Mayank Yadav <mayanky975@gmail.com>
2023-07-02 15:46:42 +0530

    Create Main.cpp

---
+++ Alcohol Detection/Main.cpp
@@ -0,0 +1,30 @@
+//Alcohol Detector
+const int MQ3=0;
+//const int LED=9;
+const int BUZZER= 10;
+int value;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(MQ3, INPUT);
+  pinMode(BUZZER, OUTPUT);//if you want you can put buzzer in place led or both in the programme
+  digitalWrite(BUZZER,LOW);
+
+}
+
+void loop()
+{
+  value= analogRead(MQ3);
+  Serial.println(value);
+  
+  if(value>500)
+  {
+    digitalWrite(BUZZER,HIGH);
+  }
+  else
+  {
+    digitalWrite(BUZZER,LOW);
+  }
+
+  delay (500);
+}
